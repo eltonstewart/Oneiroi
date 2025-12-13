246cbd6cc5a6afe90ba949f80f37106e7bc6b52f
eltonstewart <info@lazyandroid.com>
2025-12-13 11:02:14 +1100

    Improve Looper quality: Replace Linear with Cubic Hermite interpolation to reduce aliasing

--- LooperBuffer.h
+++ LooperBuffer.h
@@ -231,21 +231,64 @@ public:
         return buffer_[position];
     }
 
+    // Helper to read 4 points for Hermite interpolation
+    // p: integer position
+    // dir: direction (1 or -1)
+    inline void Read4Left(int32_t p, int32_t dir, float& xm1, float& x0, float& x1, float& x2)
+    {
+        xm1 = ReadLeft(p - dir);
+        x0  = ReadLeft(p);
+        x1  = ReadLeft(p + dir);
+        x2  = ReadLeft(p + dir * 2);
+    }
+
+    inline void Read4Right(int32_t p, int32_t dir, float& xm1, float& x0, float& x1, float& x2)
+    {
+        xm1 = ReadRight(p - dir);
+        x0  = ReadRight(p);
+        x1  = ReadRight(p + dir);
+        x2  = ReadRight(p + dir * 2);
+    }
+
     inline void Read(float p, float &left, float &right, PlaybackDirection direction = PLAYBACK_FORWARD)
     {
-        float l0, l1;
-        float r0, r1;
+        if (direction == PLAYBACK_STALLED) {
+            left = 0.f; 
+            right = 0.f;
+            return;
+        }
 
         int32_t i = int32_t(p);
-
         float f = p - i;
 
-        l0 = ReadLeft(i);
-        l1 = ReadLeft(i + direction);
-        left = l0 + direction * (l1 - l0) * f;
+        // Cubic Hermite Interpolation
+        // Requires 4 points: y[-1], y[0], y[1], y[2]
+        // In our case, relative to direction: x(i-1), x(i), x(i+1), x(i+2)
+        
+        float lm1, l0, l1, l2;
+        float rm1, r0, r1, r2;
+        int32_t dir = (int32_t)direction;
 
-        r0 = ReadRight(i);
-        r1 = ReadRight(i + direction);
-        right = r0 + direction * (r1 - r0) * f;
+        Read4Left(i, dir, lm1, l0, l1, l2);
+        Read4Right(i, dir, rm1, r0, r1, r2);
+
+        // Hermite implementation (Catmull-Rom)
+        // c0 = x0
+        // c1 = 0.5 * (x1 - xm1)
+        // c2 = xm1 - 2.5*x0 + 2*x1 - 0.5*x2
+        // c3 = 0.5*(x2 - xm1) + 1.5*(x0 - x1)
+        // out = ((c3*f + c2)*f + c1)*f + c0
+
+        // Left
+        float c1 = 0.5f * (l1 - lm1);
+        float c2 = lm1 - 2.5f * l0 + 2.f * l1 - 0.5f * l2;
+        float c3 = 0.5f * (l2 - lm1) + 1.5f * (l0 - l1);
+        left = ((c3 * f + c2) * f + c1) * f + l0;
+
+        // Right
+        c1 = 0.5f * (r1 - rm1);
+        c2 = rm1 - 2.5f * r0 + 2.f * r1 - 0.5f * r2;
+        c3 = 0.5f * (r2 - rm1) + 1.5f * (r0 - r1);
+        right = ((c3 * f + c2) * f + c1) * f + r0;
     }
 };
